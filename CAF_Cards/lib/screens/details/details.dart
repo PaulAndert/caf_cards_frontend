941fa19b8a4e5d8867e9eecb5440b5b3d5a8b3d6
PaulAndert <paulandert@web.de>
2023-02-01 14:52:10 +0100

    better looking

--- CAF_Cards/lib/screens/details/details.dart
+++ CAF_Cards/lib/screens/details/details.dart
@@ -5,406 +5,56 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
 import 'package:myapp/widgets/navbar.dart';
 
+import '../../models/ScreenArguments.dart';
+import '../../widgets/card_fullscreen_widget.dart';
+
 class Details extends StatelessWidget {
   static const String routeName = "/Details";
 
   @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
     double baseWidth = 393;
     double fem = MediaQuery.of(context).size.width / baseWidth;
     double ffem = fem * 0.97;
-    return Container(
-      width: double.infinity,
+    return Material(
       child: Container(
-        // detailsrPf (10:10948)
-        padding: EdgeInsets.fromLTRB(9*fem, 31*fem, 9*fem, 0*fem),
         width: double.infinity,
-        decoration: BoxDecoration (
+        height: double.infinity,
+        decoration: const BoxDecoration(
           color: Color(0xff202024),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              // autogroup7cpfu73 (T1TKpYRzPZaQqwv6jw7cPF)
-              margin: EdgeInsets.fromLTRB(25*fem, 0*fem, 28*fem, 57*fem),
-              width: double.infinity,
+            Expanded(flex: 1, child: Container()),
+            Expanded(
+              flex: 1,
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container(
-                    // iconographycaesarzknaU5 (26:970)
-                    margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 250*fem, 2*fem),
-                    child: TextButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom (
-                        padding: EdgeInsets.zero,
-                      ),
-                      child: Container(
-                        width: 32*fem,
-                        height: 32*fem,
-                        child: Image.asset(
-                          'assets/page-1/images/iconography-caesarzkn-ee1.png',
-                          width: 32*fem,
-                          height: 32*fem,
-                        ),
-                      ),
-                    ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.arrow_back_ios,
+                        color: Color(0xffffffff)),
                   ),
-                  TextButton(
-                    // iconsPgR (26:982)
+                  IconButton(
                     onPressed: () {},
-                    style: TextButton.styleFrom (
-                      padding: EdgeInsets.zero,
-                    ),
-                    child: Container(
-                      width: 40*fem,
-                      height: 40*fem,
-                      child: Image.asset(
-                        'assets/page-1/images/icons-LeH.png',
-                        width: 40*fem,
-                        height: 40*fem,
-                      ),
-                    ),
+                    icon: const Icon(Icons.edit, color: Color(0xffffffff)),
                   ),
                 ],
               ),
             ),
-            Container(
-              // group6f89 (111:2363)
-              margin: EdgeInsets.fromLTRB(9*fem, 0*fem, 6.14*fem, 61*fem),
-              width: double.infinity,
-              height: 576*fem,
-              child: Stack(
-                children: [
-                  Positioned(
-                    // rectangle38Jgu (111:2364)
-                    left: 0*fem,
-                    top: 0*fem,
-                    child: Align(
-                      child: SizedBox(
-                        width: 357*fem,
-                        height: 576*fem,
-                        child: Container(
-                          decoration: BoxDecoration (
-                            borderRadius: BorderRadius.circular(29*fem),
-                            color: Color(0xff47479d),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // group9koo (111:2366)
-                    left: 292.9998779297*fem,
-                    top: 3*fem,
-                    child: Align(
-                      child: SizedBox(
-                        width: 66.86*fem,
-                        height: 66.86*fem,
-                        child: Image.asset(
-                          'assets/page-1/images/group-9-PCH.png',
-                          width: 66.86*fem,
-                          height: 66.86*fem,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // oXB (111:2370)
-                    left: 285*fem,
-                    top: 22.5*fem,
-                    child: Center(
-                      child: Align(
-                        child: SizedBox(
-                          width: 26*fem,
-                          height: 22*fem,
-                          child: Text(
-                            '2',
-                            textAlign: TextAlign.center,
-                            style: SafeGoogleFont (
-                              'SF Pro Display',
-                              fontSize: 48*ffem,
-                              fontWeight: FontWeight.w700,
-                              height: 0.4583333333*ffem/fem,
-                              color: Color(0xffffffff),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // roundstartget1nP7 (111:2371)
-                    left: 122*fem,
-                    top: 396.5*fem,
-                    child: Center(
-                      child: Align(
-                        child: SizedBox(
-                          width: 110*fem,
-                          height: 44*fem,
-                          child: Text(
-                            'Round start:\nGet 1  .',
-                            textAlign: TextAlign.center,
-                            style: SafeGoogleFont (
-                              'SF Pro Display',
-                              fontSize: 20*ffem,
-                              fontWeight: FontWeight.w700,
-                              height: 1.1*ffem/fem,
-                              color: Color(0xffffffff),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // topfdergierb5f (111:2372)
-                    left: 28*fem,
-                    top: 22.5*fem,
-                    child: Align(
-                      child: SizedBox(
-                        width: 89*fem,
-                        height: 22*fem,
-                        child: Text(
-                          'Wall-E',
-                          style: SafeGoogleFont (
-                            'SF Pro Display',
-                            fontSize: 32*ffem,
-                            fontWeight: FontWeight.w700,
-                            height: 0.6875*ffem/fem,
-                            color: Color(0xffffffff),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // asmallrobotthatlovestocleanqkh (111:2373)
-                    left: 28*fem,
-                    top: 291.5*fem,
-                    child: Align(
-                      child: SizedBox(
-                        width: 287*fem,
-                        height: 22*fem,
-                        child: Text(
-                          'A small robot that loves to clean.',
-                          style: SafeGoogleFont (
-                            'SF Pro Display',
-                            fontSize: 20*ffem,
-                            fontWeight: FontWeight.w700,
-                            height: 1.1*ffem/fem,
-                            color: Color(0xffffffff),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // grafik4fUq (111:2374)
-                    left: 28*fem,
-                    top: 66*fem,
-                    child: Align(
-                      child: SizedBox(
-                        width: 302*fem,
-                        height: 202*fem,
-                        child: Image.asset(
-                          'assets/page-1/images/grafik-4-yTT.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // group15kWH (111:2375)
-                    left: 11*fem,
-                    top: 522.1428222656*fem,
-                    child: Container(
-                      width: 64.58*fem,
-                      height: 46.36*fem,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Center(
-                            // aVK (111:2381)
-                            child: Container(
-                              margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 5.74*fem, 1.65*fem),
-                              child: Text(
-                                '1',
-                                textAlign: TextAlign.center,
-                                style: SafeGoogleFont (
-                                  'SF Pro Display',
-                                  fontSize: 48*ffem,
-                                  fontWeight: FontWeight.w700,
-                                  height: 0.4583333333*ffem/fem,
-                                  color: Color(0xffffffff),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            // group7DoB (111:2376)
-                            width: 32.83*fem,
-                            height: 46.36*fem,
-                            child: Image.asset(
-                              'assets/page-1/images/group-7-FWM.png',
-                              width: 32.83*fem,
-                              height: 46.36*fem,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // group14iE9 (111:2382)
-                    left: 269*fem,
-                    top: 521*fem,
-                    child: Container(
-                      width: 81*fem,
-                      height: 50*fem,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Center(
-                            // yfs (111:2383)
-                            child: Container(
-                              margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 5*fem, 1*fem),
-                              child: Text(
-                                '2',
-                                textAlign: TextAlign.center,
-                                style: SafeGoogleFont (
-                                  'SF Pro Display',
-                                  fontSize: 48*ffem,
-                                  fontWeight: FontWeight.w700,
-                                  height: 0.4583333333*ffem/fem,
-                                  color: Color(0xffffffff),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            // group83vd (111:2384)
-                            width: 50*fem,
-                            height: 50*fem,
-                            child: Image.asset(
-                              'assets/page-1/images/group-8-FpZ.png',
-                              width: 50*fem,
-                              height: 50*fem,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // group16LPw (111:2388)
-                    left: 22*fem,
-                    top: 346*fem,
-                    child: Align(
-                      child: SizedBox(
-                        width: 309*fem,
-                        height: 145*fem,
-                        child: Image.asset(
-                          'assets/page-1/images/group-16-Bxq.png',
-                          width: 309*fem,
-                          height: 145*fem,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // pictureyho (111:2394)
-                    left: 151.5*fem,
-                    top: 90.5*fem,
-                    child: Center(
-                      child: Align(
-                        child: SizedBox(
-                          width: 53*fem,
-                          height: 22*fem,
-                          child: Text(
-                            'picture',
-                            textAlign: TextAlign.center,
-                            style: SafeGoogleFont (
-                              'SF Pro Text',
-                              fontSize: 17*ffem,
-                              fontWeight: FontWeight.w600,
-                              height: 1.2941176471*ffem/fem,
-                              color: Color(0xff000000),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // walle111Pb (111:2395)
-                    left: 31*fem,
-                    top: 66*fem,
-                    child: Align(
-                      child: SizedBox(
-                        width: 299*fem,
-                        height: 202*fem,
-                        child: Image.asset(
-                          'assets/page-1/images/walle1-1-ivR.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // group236A9 (111:2396)
-                    left: 188*fem,
-                    top: 415*fem,
-                    child: Align(
-                      child: SizedBox(
-                        width: 33*fem,
-                        height: 33*fem,
-                        child: Image.asset(
-                          'assets/page-1/images/group-23-DqF.png',
-                          width: 33*fem,
-                          height: 33*fem,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            Expanded(
+              flex: 10,
+              child: card_fullscreen_widget(fem: fem, ffem: ffem, args: args),
             ),
-            Container(
-              // tabbarios5tabssfAvh (I10:11721;10:2672)
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Navbar(fem: fem),
-                  Container(
-                    // ioshomeindicatorsfnpu (I10:11721;10:2672;17:132)
-                    padding: EdgeInsets.fromLTRB(121*fem, 21*fem, 120*fem, 8*fem),
-                    width: double.infinity,
-                    decoration: BoxDecoration (
-                      color: Color(0xff202024),
-                    ),
-                    child: Center(
-                      // homeindicatorgfP (I10:11721;10:2672;17:132;2:2)
-                      child: SizedBox(
-                        width: double.infinity,
-                        height: 5*fem,
-                        child: Container(
-                          decoration: BoxDecoration (
-                            borderRadius: BorderRadius.circular(100*fem),
-                            color: Color(0xffffffff),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            Expanded(flex: 3, child: Container()),
           ],
         ),
       ),
-          );
+    );
   }
 }
--- CAF_Cards/lib/screens/trade/confirm_card/trading-window-ifX.dart
+++ CAF_Cards/lib/screens/trade/confirm_card/trading-window-ifX.dart
@@ -40,62 +40,45 @@ class _TradeConfirmCardState extends State<TradeConfirmCard> {
     return Material(
       child: Container(
         width: double.infinity,
-        child: Container(
-          // tradingwindowpFw (26:577)
-          padding: EdgeInsets.fromLTRB(6 * fem, 56 * fem, 3 * fem, 15 * fem),
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            color: Color(0xff202024),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                // group4fnM (103:1271)
-                margin:
-                    EdgeInsets.fromLTRB(2 * fem, 0 * fem, 0 * fem, 16 * fem),
-                width: 382 * fem,
-                height: 66 * fem,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15 * fem),
-                ),
-                child: Stack(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: const Icon(Icons.arrow_back_ios,
-                                color: Color(0xffffffff)),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              updateTradeCardId(args.deviceId, args.card.id);
-                              Navigator.pop(context);
-                              Navigator.pushNamed(
-                                  context, TradingConfirmTrade.routeName,
-                                  arguments: args);
-                            },
-                            icon: const Icon(Icons.check,
-                                color: Color(0xffffffff)),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          color: Color(0xff202024),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(flex: 1, child: Container()),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.arrow_back_ios,
+                        color: Color(0xffffffff)),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      updateTradeCardId(args.deviceId, args.card.id);
+                      Navigator.pop(context);
+                      Navigator.pushNamed(
+                          context, TradingConfirmTrade.routeName,
+                          arguments: args);
+                    },
+                    icon: const Icon(Icons.check, color: Color(0xffffffff)),
+                  ),
+                ],
               ),
-              Expanded(
-                flex: 10,
-                child: card_fullscreen_widget(fem: fem, ffem: ffem, args: args),
-              ),
-            ],
-          ),
+            ),
+            Expanded(
+              flex: 10,
+              child: card_fullscreen_widget(fem: fem, ffem: ffem, args: args),
+            ),
+            Expanded(flex: 2, child: Container()),
+          ],
         ),
       ),
     );
