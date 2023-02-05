8b525a6b84e4b4ae75786f5302032fc65ece5390
PaulAndert <paulandert@web.de>
2023-02-05 16:15:25 +0100

    changes

--- CAF_Cards/lib/screens/create/preview/preview.dart
+++ CAF_Cards/lib/screens/create/preview/preview.dart
@@ -5,7 +5,6 @@ import '../../../models/Gamecard.dart';
 import '../../../models/screen_argument.dart';
 import '../../../widgets/big_card.dart';
 
-
 class CreatePreview extends StatefulWidget {
   const CreatePreview({super.key});
 
@@ -16,37 +15,61 @@ class CreatePreview extends StatefulWidget {
 }
 
 class _CreatePreview extends State<CreatePreview> {
-
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as ScreenArgument;
     double baseWidth = 393;
     double fem = MediaQuery.of(context).size.width / baseWidth;
     double ffem = fem * 0.97;
-    return Container(
-      width: double.infinity,
+    return Material(
       child: Container(
-        padding: EdgeInsets.fromLTRB(9 * fem, 42 * fem, 9 * fem, 0 * fem),
         width: double.infinity,
-        decoration: const BoxDecoration(
-          color: Color(0xff202024),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 1,
-              child: Container(),
-            ),
-            // Card
-            Expanded(
-              flex: 25,
-              child: BigCard(
-                fem: fem,
-                ffem: ffem,
-                args: ScreenArgument(
-                  args.deviceId,
-                  Gamecard(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(9 * fem, 42 * fem, 9 * fem, 0 * fem),
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            color: Color(0xff202024),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(),
+              ),
+              Expanded(
+                  flex: 2,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // Button back
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.white,
+                            )),
+                      ),
+                      // Points
+                      Expanded(
+                        flex: 1,
+                        child: Container(),
+                      ),
+                    ],
+                  )),
+              // Card
+              Expanded(
+                flex: 15,
+                child: BigCard(
+                  fem: fem,
+                  ffem: ffem,
+                  args: ScreenArgument(
+                    args.deviceId,
+                    Gamecard(
                       id: 0,
                       name: args.card.name,
                       description: args.card.description,
@@ -55,44 +78,49 @@ class _CreatePreview extends State<CreatePreview> {
                       health: args.card.health,
                       userIds: [],
                       abilityId: args.card.abilityId,
+                    ),
+                    args.ability,
                   ),
-                  args.ability,
                 ),
               ),
-            ),
-            // Confirm Button
-            Expanded(
-              flex: 4,
-              child: Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/CreatePreview');
-                  },
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.zero,
-                    backgroundColor: Colors.deepPurpleAccent,
-                  ),
-                  child: Container(
-                    width: 200 * fem,
-                    height: 50 * fem,
-                    child: Center(
-                      child: Text(
-                        "Confirm",
-                        textAlign: TextAlign.center,
-                        style: SafeGoogleFont(
-                          'SF Pro Display',
-                          fontSize: 17 * ffem,
-                          fontWeight: FontWeight.w700,
-                          height: 1.2941176471 * ffem / fem,
-                          color: const Color(0xffffffff),
+              // Confirm Button
+              Expanded(
+                flex: 4,
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/CreatePreview');
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      backgroundColor: Colors.deepPurpleAccent,
+                    ),
+                    child: Container(
+                      width: 200 * fem,
+                      height: 50 * fem,
+                      child: Center(
+                        child: Text(
+                          "Confirm",
+                          textAlign: TextAlign.center,
+                          style: SafeGoogleFont(
+                            'SF Pro Display',
+                            fontSize: 17 * ffem,
+                            fontWeight: FontWeight.w700,
+                            height: 1.2941176471 * ffem / fem,
+                            color: const Color(0xffffffff),
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              Expanded(
+                flex: 1,
+                child: Container(),
+              ),
+            ],
+          ),
         ),
       ),
     );
